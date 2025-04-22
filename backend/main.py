069d6dfa1d0beba9f2e6791d69bda9e83f310401
meedrifai <rifaii.mohameed@gmail.com>
2025-04-23 00:16:38 +0100

    go

--- backend/main.py
+++ backend/main.py
@@ -74,4 +74,4 @@ def predict(input_batch: BatchInput):
 if __name__ == "__main__":
     print("Starting server...")
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
